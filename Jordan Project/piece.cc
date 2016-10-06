c887b818534ca830fa41c379382db357281de495
Jirby7 <jirby7@gmail.com>
2016-10-06 12:49:19 -0400

    piece.cc

---
+++ Jordan Project/piece.cc
@@ -0,0 +1,38 @@
+#include "piece.h"
+
+// Constructor.
+void Piece::flip()
+{
+  if (color == "B") color = "W";
+  else color = "B";
+}
+
+// Returns true if the piece is blank
+bool Piece::is_empty() const
+{
+  return (color == "");
+}
+
+// Returns true if the piece is black.
+bool Piece::is_black() const
+{
+  return (color == "B");
+}
+
+// Returns true if the piece is white.
+bool Piece::is_white() const
+{
+  return (color == "W");
+}
+
+// Operator != Compares two objects of type Piece.
+bool Piece::operator!= (const Piece & rhs) const
+{
+  return (color != rhs.color);
+}
+
+// Operator == Compares two objects of type Piece.
+bool Piece::operator== (const Piece & rhs) const
+{
+  return (color == rhs.color);
+}
